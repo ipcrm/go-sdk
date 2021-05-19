cee594fb98ca21e7fbfc716dbade2ce40ac9523d
ipcrm <mcadorette@gmail.com>
2021-05-19 10:40:43 -0400

    ALLY-465 (wip) Add new table construction/rendering model
    In support of ALLY-465, update the way tables are created.  Prior to
    this commit, tables were built and rendered as strings immediately.  For
    ALLY-465 (the first introduction of CSV output to the cli) we need to
    delay the rendering of tables so that the same data can be used to
    render a human readable table, or a CSV.

--- cli/cmd/table_render.go
+++ cli/cmd/table_render.go
@@ -24,6 +24,113 @@ import (
 	"github.com/olekukonko/tablewriter"
 )
 
+type Table struct {
+	headers     []string
+	data        [][]string
+	innerTables []Table
+	opts        []tableOption
+	label       string
+	renderAsCsv bool
+}
+
+// Apply any custom options
+func (t *Table) ApplyOpts(tbl *tablewriter.Table) {
+	for _, opt := range t.opts {
+		opt.apply(tbl)
+	}
+}
+
+// Testing
+func (t *Table) Render() string {
+	// TODO
+	// validate only data or innerTables supplied
+	var returnString string
+
+	if t.renderAsCsv {
+		return t.RenderAsCSV()
+	}
+
+	if len(t.innerTables) == 0 {
+		var (
+			tblBldr = &strings.Builder{}
+			tbl     = tablewriter.NewWriter(tblBldr)
+		)
+		tbl.SetHeader(t.headers)
+
+		// Custom table
+		if len(t.opts) > 0 {
+			t.ApplyOpts(tbl)
+		} else {
+			// Simple table
+			tbl.SetRowLine(false)
+			tbl.SetBorder(false)
+			tbl.SetAutoWrapText(true)
+			tbl.SetAlignment(tablewriter.ALIGN_LEFT)
+			tbl.SetColumnSeparator(" ")
+		}
+
+		tbl.AppendBulk(t.data)
+		tbl.Render()
+		returnString = tblBldr.String()
+	} else {
+		var (
+			tblBldr = &strings.Builder{}
+			tbl     = tablewriter.NewWriter(tblBldr)
+		)
+		tbl.SetHeader(t.headers)
+		t.ApplyOpts(tbl)
+		tblRows := []string{}
+		for _, it := range t.innerTables {
+			tblRows = append(tblRows, it.Render())
+		}
+
+		tbl.AppendBulk([][]string{tblRows})
+		tbl.Render()
+		returnString = tblBldr.String()
+	}
+
+	return returnString
+}
+
+func (t *Table) Headers() []string {
+	var newHeaders []string
+	for _, h := range t.headers {
+		newHeaders = append(newHeaders, strings.Replace(h, "\n", "", -1)+"\n")
+	}
+	return newHeaders
+}
+
+func (t *Table) Data() [][]string {
+	var newData [][]string
+	for _, d := range t.data {
+		var newInnerData []string
+		for _, id := range d {
+			newInnerData = append(newInnerData, strings.Replace(id, "\n", "", -1))
+		}
+		newData = append(newData, newInnerData)
+	}
+	return newData
+}
+
+// Testing
+func (t *Table) RenderAsCSV() string {
+	outstring := ""
+	if len(t.innerTables) == 0 {
+		outstring = renderTableAsCSV(t.Headers(), t.Data())
+	} else {
+		csvOutRaw := []string{}
+
+		for idx, it := range t.innerTables {
+			csvOutRaw = append(csvOutRaw, t.Headers()[idx])
+			csvOutRaw = append(csvOutRaw, it.RenderAsCSV())
+			csvOutRaw = append(csvOutRaw, "\n")
+		}
+		outstring = strings.Join(csvOutRaw, "")
+	}
+
+	return outstring
+}
+
 // renderSimpleTable is used to render any simple table within the Lacework CLI,
 // every command should leverage this function unless there are extra customizations
 // required, if so, use instead renderCustomTable(). The benefit of this function
--- cli/cmd/table_render_test.go
+++ cli/cmd/table_render_test.go
@@ -120,3 +120,120 @@ func TestRenderCustomTable(t *testing.T) {
 		expectedTable,
 		"tables are not being formatted correctly")
 }
+
+func TestSimpleTable(t *testing.T) {
+	expectedTable := strings.TrimPrefix(`
+  KEY    VALUE   
+-------+---------
+  key1   value1  
+  key2   value2  
+  key3   value3  
+`, "\n")
+
+  table := Table{
+		headers: []string{"KEY", "VALUE"},
+		data: [][]string{
+						{"key1", "value1"},
+						{"key2", "value2"},
+						{"key3", "value3"},
+					},
+  }
+
+	assert.Equal(t,
+	  table.Render(),
+		expectedTable,
+		"tables are not being formatted correctly")
+}
+
+func TestSimpleTableLongDescriptions(t *testing.T) {
+	expectedTable := strings.TrimPrefix(`
+  ID            DESCRIPTION            
+-----+---------------------------------
+  1    This is a long long very        
+       long description that will be   
+       splitted into multiple lines    
+  2    No a very long description      
+`, "\n")
+
+	table := Table{
+		headers: []string{"ID", "Description"},
+		data: [][]string{
+			{"1", "This is a long long very long description that will be splitted into multiple lines"},
+			{"2", "No a very long description"},
+		},
+	}
+
+	assert.Equal(t,
+	  table.Render(),
+		expectedTable,
+		"tables are not being formatted correctly")
+}
+
+func TestCustomTable(t *testing.T) {
+	detailsTable := [][]string{
+		{"KEY1", "VALUE1"},
+		{"KEY2", "VALUE2"},
+		{"KEY3", "VALUE3"},
+	}
+	summaryTable := [][]string{
+		{"Severity1", "1"},
+		{"Secerity2", "2"},
+		{"Secerity3", "0"},
+	}
+	expectedTable := strings.TrimPrefix(`
+   REPORT DETAILS       RECOMMENDATIONS     
+-------------------+------------------------
+    KEY1  VALUE1       SEVERITY    COUNT    
+    KEY2  VALUE2     ------------+--------  
+    KEY3  VALUE3       Severity1       1    
+                       Secerity2       2    
+                       Secerity3       0    
+                                            
+`, "\n")
+
+	deets := Table{
+		headers: []string{},
+		data: detailsTable,
+		opts: []tableOption{
+					tableFunc(func(t *tablewriter.Table) {
+						t.SetBorder(false)
+						t.SetColumnSeparator("")
+						t.SetAlignment(tablewriter.ALIGN_LEFT)
+					}),
+		},
+	}
+
+	summary := Table{
+		headers: []string{"Severity", "Count"},
+		data: summaryTable,
+		opts: []tableOption{
+					tableFunc(func(t *tablewriter.Table) {
+						t.SetBorder(false)
+						t.SetColumnSeparator(" ")
+					}),
+		},
+	}
+
+	table := Table{
+		headers: []string{
+				"Report Details",
+				"Recommendations",
+  	},
+		innerTables: []Table{
+			deets,
+			summary,
+		},
+		opts: []tableOption{
+			tableFunc(func(t *tablewriter.Table) {
+				t.SetBorder(false)
+				t.SetAutoWrapText(false)
+				t.SetColumnSeparator(" ")
+			}),
+		},
+	}
+
+	assert.Equal(t,
+  	table.Render(),
+		expectedTable,
+		"tables are not being formatted correctly")
+}
